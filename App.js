838ef840d7a967492efa8efa845ef0451acee484
JamiNorja <jami.norja@outlook.com>
2023-09-04 14:10:17 +0300

    Muokattu

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [number1, setNumber1] = useState('');
   const [number2, setNumber2] = useState('');
-  
+  const [result, setResult] = useState(''); 
+
+  const initialFocus = useRef(null);
 
   const plus = () => {
-     result = parseInt(number1) + parseInt(number2);
-     console.log(result);
+     setResult(parseInt(number1) + parseInt(number2));
+     setNumber1('');
+     setNumber2('');
+     initialFocus.current.focus();
      return result;
   };
 
   const minus = () => { 
-    result = parseInt(number1) - parseInt(number2);
-    console.log(result);
+    setResult(parseInt(number1) - parseInt(number2));
+    setNumber1('');
+    setNumber2('');
+    initialFocus.current.focus();
     return result;
   };
 
   return (
     <View style={styles.container}>
       <Text style={{fontSize: 20}}>Result: {result}</Text> 
-      <TextInput style={styles.input} onChangeText={number1 => setNumber1(number1)} value={number1} keyboardType='number-pad'/>
+      <TextInput style={styles.input} ref={initialFocus} onChangeText={number1 => setNumber1(number1)} value={number1} keyboardType='number-pad'/>
       <TextInput style={styles.input} onChangeText={number2 => setNumber2(number2)} value={number2} keyboardType='number-pad'/>
       <View style={styles.button}>
         <Button onPress={plus} title="+" />
@@ -36,20 +42,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 250,
-    alignItems: 'center'
+    flex: 1,
+    backgroundColor: 'afff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   input : {
-    width:150,
+    width:'50%',
     borderColor: 'gray', 
-    borderWidth: 1
+    borderWidth: 1,
+    padding: 5,
+    margin: 5,
   },
   button : {
-    marginTop: 20,
-    alignItems: 'center',
+    width: '30%',
     flexDirection: 'row',
-    paddingLeft: 150,
-    paddingRight: 150,
-    justifyContent: 'space-around'
+    justifyContent: 'space-evenly',
   },
 });
